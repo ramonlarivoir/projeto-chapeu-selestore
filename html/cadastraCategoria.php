da8dbc537d91c47e384a362eaa88d18737137a27
AndreLuiz123 <luizandr30@yahoo.com>
2018-06-13 20:52:53 -0300

    faz formulario de cadastro de nova categoria

---
+++ html/cadastraCategoria.php
@@ -0,0 +1,51 @@
+<?php
+    include("navbar-admin.php");
+?>
+
+      <div class = "container">
+          <div class ="row">
+
+            <form action="cadastroCategoria.php" method="post" class="form-admin-produto">
+              <label for="id_categoria">ID da nova categoria:</label>
+              <input type="text" name="id_categoria" id="id_categoria" class="form-control"/required>
+              <label for="nomeCategoria">Nome da nova categoria:</label>
+              <input type="text" name="nomeCategoria" id="nomeCategoria" class="form-control"/required>
+
+               <button class="mt-3 btn btn-success " type="submit">Salvar</button>
+
+               <?php
+               $server = 'localhost';
+               $user = 'root';
+               $password = 'root';
+               $port = '3306';
+               $nomeBancoDados = 'chapeuseletor';
+
+               $db_connect = new mysqli($server, $user, $password, $nomeBancoDados, $port);
+
+               if($db_connect->connect_error==true){
+                 echo 'falha na conexão'.$db_connect->connect_error;
+               }else{
+              //    echo 'conectou';
+
+                 $nome = $_POST["nomeCategoria"];
+
+                 $id = $_POST["id_categoria"];
+
+                 if($id!=NULL && $nome!=NULL){
+                   $sql2 = "INSERT INTO categoria(id_categoria,nome_categoria) VALUES ($nome,$id)";
+
+                     $db_connect->query($sql);
+                 }
+
+               }
+                ?>
+              </form>
+
+            </div>
+
+               <a class="mt-3 mb-3 btn btn-primary" href="produtosTeste.html" role="button">Voltar</a>
+
+         </div>
+<?php
+    include("footer-admin.php");
+?>
