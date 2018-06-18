94ee6cbd6b70c14af7133c1e4b5757c0c4482304
AndreLuiz123 <luizandr30@yahoo.com>
2018-06-18 18:11:05 -0300

    atualiza paginas

--- html/cadastraCategoria.php
+++ html/cadastraCategoria.php
@@ -8,8 +8,8 @@
 
             <form action="cadastraCategoria.php" method="post" class="form-admin-produto">
               <label for="nomeC">Nome da Categoria:</label>
-              <input type="text" name="nomeC" id="nomeC" class="form-control"/required>
-              <label for="id_categoria">Novo ID:</label>
+            <!--  <input type="text" name="nomeC" id="nomeC" class="form-control"/required>
+              <label for="id_categoria">Novo ID:</label>-->
               <input type="text" name="id_categoria" id="id_categoria" class="form-control"/required>
 
 
@@ -23,32 +23,41 @@
                $password = 'root';
                $port = '3306';
                $nomeBancoDados = 'chapeuseletor';
-
                $db_connect = new mysqli($server, $user, $password, $nomeBancoDados, $port);
 
 
                $nome = $_POST["nomeC"];
+          //     $id = $_POST["id_categoria"];
 
-               $id = $_POST["id_categoria"];
+               $sqlConsultaID = "SELECT id_categoria FROM categoria";
+
+               $resultado = mysqli_query($db_connect,$sqlConsultaID);
+
+
+
+
+
+
+
+               while()
+
+
+
+
 
                if($db_connect->connect_error==true){
                  echo 'falha na conexão'.$db_connect->connect_error;
                }else{
-                //  echo 'conectou';
+                  echo 'conectou';
 
                if($id!=NULL && $nome!=NULL){
-                // echo "ooi";
-                 $sql2 = "INSERT INTO categoria(id_categoria,nome_categoria) VALUES ('$id','$nome')";
 
+                   $sql2 = "INSERT INTO categoria(id_categoria,nome_categoria) VALUES (null,'$nome')";
                    $db_connect->query($sql2);
+
                }
 
              }
-
-            //   echo $nome;
-            //   echo $id;
-            //   echo $descricao;
-
 
                 ?>
 
