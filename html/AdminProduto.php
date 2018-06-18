938969289db7ec47471eaba73dff561884fbfb9a
AndreLuiz123 <luizandr30@yahoo.com>
2018-06-17 23:05:25 -0300

    consegue editar informações de objeto da classe produtos

--- html/AdminProduto.php
+++ html/AdminProduto.php
@@ -11,16 +11,46 @@
             </div>
 
 
-            <form action="" class="form-admin-produto">
-              <label for="nome">Nome:</label>
-              <input type="text" name="nome" id="nome" class="form-control" >
+            <form action="AdminProduto.php" method="post" class="form-admin-produto">
+              <label for="nomeC">Nome:</label>
+              <input type="text" name="nomeC" id="nomeC" class="form-control"/required>
               <label for="preço">Preço:</label>
-              <input type="text" name="preço" id="preço" class="form-control" >
+              <input type="text" name="preço" id="preço" class="form-control"/required>
 
               <label for="descrição">Descrição:</label>
-              <textarea class="form-control" rows="3"></textarea>
+              <textarea class="form-control" rows="3"/required></textarea>
 
-               <button class="mt-3 btn btn-success ">Salvar</button>
+               <button class="mt-3 btn btn-success " type="submit">Salvar</button>
+
+               <?php
+
+               $server = 'localhost';
+               $user = 'root';
+               $password = 'root';
+               $port = '3306';
+               $nomeBancoDados = 'chapeuseletor';
+
+               $db_connect = new mysqli($server, $user, $password, $nomeBancoDados, $port);
+
+               $nome = $_POST["nomeC"];
+
+               $preco = $_POST["preço"];
+
+               $resultadoNome = "UPDATE produto SET nome_produto = '$nome' WHERE id_produto=1";
+               $resultadoPreco = "UPDATE produto SET preco = $preco WHERE id_produto=1";
+
+
+
+
+               $db_connect->query($resultadoNome);
+               $db_connect->query($resultadoPreco);
+
+               echo 'chapeuSeletor';
+               echo $preco;
+            //   echo $descricao;
+
+
+                ?>
 
 
               </form>
