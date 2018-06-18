81d20e7b487e4f929c72d3b5788573c44b677373
KemSasaoka <sasaoka.igor@gmail.com>
2018-06-18 12:21:57 -0300

    finalizado a parte de ediçao e deleção de usuário

--- html/tabela-usuarios.php
+++ html/tabela-usuarios.php
@@ -1,5 +1,23 @@
 <?php include("navbar-admin.php");?>
 <?php include("conexao.php");?>
+<?php if($_SERVER['REQUEST_METHOD']=='POST'){
+        $id = $_POST['id'];
+        $query = "DELETE from usuario WHERE id_usuario ='$id'";
+        $resultado = $conexao->query($query);
+        if($resultado){ 
+            echo 
+                '<div class="alert alert-success text-center" role="alert">
+                    AVISO: USUÁRIO DELETADO COM SUCESSO!!!
+                </div>';
+
+        }else{
+            echo 
+                '<div class="alert alert-danger text-center" role="alert">
+                    AVISO: ERRO AO TENTAR DELETAR USUARIO!!!
+                </div>';
+        }
+    }
+?>
 <div class="container">
     <table class="table table-striped table-texto">
         <thead>
@@ -16,6 +34,7 @@
                 $query = "SELECT * from usuario";
                 $resultado = $conexao->query($query);
                 if($resultado >0){
+                    $contador = 0;
                     while($row = $resultado->fetch_assoc()){
                         $nomeUsuario = $row['login'];
                         $idUsuario = $row['id_usuario'];
@@ -25,7 +44,7 @@
                                 <td>'.$idUsuario.'</th>
                                     <td>'.$nomeUsuario.'</td>
                                     <td>
-                                        <a href="" role="button" data-toggle="modal" data-target="#excluirUsuario">
+                                        <a href="" role="button" data-toggle="modal" data-target="#excluirUsuario'.$contador.'">
                                             <img src="../assets/bootstrap/icons/png/trash-2x.png">
                                         </a>
                                     </td>
@@ -35,7 +54,7 @@
                                         </a>
                                 </td>
                             </tr>
-                            <div class="modal fade" id="excluirUsuario" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="excluirUsuario'.$contador.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
                                 <div class="modal-content">
                                 
@@ -51,14 +70,15 @@
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                    
-                                        <button href="" type="submit" class="btn btn-danger">Excluir Usuário</button>
-                                    
+                                    <form method="post" action="tabela-usuarios.php">
+                                        <button href="" type="submit" name ="id" value="'.$idUsuario.'" class="btn btn-danger">Excluir Usuário</button>
+                                    </form>
                                 </div>
                                 </div>
                             </div>
                             </div>
                             ';
+                            $contador++;
                     }
                 }
                 mysqli_close($conexao);
