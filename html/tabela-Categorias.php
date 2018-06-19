e3851f84293709508667dd34803c632f855b3735
AndreLuiz123 <luizandr30@yahoo.com>
2018-06-18 22:53:09 -0300

    add tabela de categorias

---
+++ html/tabela-Categorias.php
@@ -0,0 +1,99 @@
+
+<?php include("navbar-admin.php");?>
+<?php
+$server = 'localhost';
+$user = 'root';
+$password = 'root';
+$port = '3306';
+$nomeBancoDados = 'chapeuseletor';
+
+$db_connect = new mysqli($server, $user, $password, $nomeBancoDados, $port);
+?>
+<?php if($_SERVER['REQUEST_METHOD']=='POST'){
+        $id = $_POST['id'];
+        $query = "DELETE from categoria WHERE id_categoria ='$id'";
+        $resultado = $db_connect->query($query);
+        if($resultado){
+            echo
+                '<div class="alert alert-success text-center" role="alert">
+                    AVISO: CATEGORIA DELETADO COM SUCESSO!!!
+                </div>';
+        }else{
+            echo
+                '<div class="alert alert-danger text-center" role="alert">
+                    AVISO: ERRO AO TENTAR DELETAR CATEGORIA!!!
+                </div>';
+        }
+    }
+?>
+<div class="container">
+    <table class="table table-striped table-texto">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Categoria</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $query = "SELECT * from categoria";
+                $resultado = $db_connect->query($query);
+                if($resultado >0){
+                    $contador = 0;
+                    while($row = $resultado->fetch_assoc()){
+                        $nomeCategoria = $row['nome_categoria'];
+                        $idCategoria = $row['id_categoria'];
+                        echo
+                            '
+                            <tr>
+                                <td>'.$idCategoria.'</th>
+                                    <td>'.$nomeCategoria.'</td>
+                                    <td>
+                                        <a href="" role="button" data-toggle="modal" data-target="#excluirCategoria'.$contador.'">
+                                            <img src="../assets/bootstrap/icons/png/trash-2x.png">
+                                        </a>
+                                    </td>
+                                    <td>
+                                        <a href="editar_Categoria.php?id='.$idCategoria.'">
+                                            <img src="../assets/bootstrap/icons/png/cog-2x.png">
+                                        </a>
+                                </td>
+                            </tr>
+                            <div class="modal fade" id="excluirCategoria'.$contador.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">AVISO</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+
+                                <div class="modal-body">
+                                    Tem certeza que deseja excluir este categoria?
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                    <form method="post" action="tabela-Categorias.php">
+                                        <button href="" type="submit" name ="id" value="'.$idCategoria.'" class="btn btn-danger">Excluir categoria</button>
+                                    </form>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                            ';
+                            $contador++;
+                    }
+                }
+                mysqli_close($db_connect);
+            ?>
+
+
+        </tbody>
+    </table>
+</div>
+<?php include("footer-admin.php");?>
