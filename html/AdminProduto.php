d94728d15c1751a95227665f1153f5c52ed27a1b
AndreLuiz123 <luizandr30@yahoo.com>
2018-06-21 02:00:20 -0300

    edita imagem do admin produto e salva link dela no BD

--- html/AdminProduto.php
+++ html/AdminProduto.php
@@ -2,6 +2,7 @@
     include("navbar-admin.php");
 ?>
 
+
 <?php
 
 $server = 'localhost';
@@ -11,62 +12,105 @@ $port = '3306';
 $nomeBancoDados = 'chapeuseletor';
 $db_connect = new mysqli($server, $user, $password, $nomeBancoDados, $port);
 
-
-        $nome = $_POST["nomeC"];
-        $preco = $_POST["preço"];
-        $descricao = $_POST["descricao"];
-
-         $id = $_GET['id'];
-
-
 ?>
+
+<?php
+
+  if(isset($_POST['submit'])){
+    $file = $_FILES['file'];
+    $fileName = $_FILES['file']['name'];
+    $fileTemporaryName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+
+
+    $fileExt = explode('.',$fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg','jpeg','png');
+
+    if(in_array($fileActualExt,$allowed)){
+      if($fileError === 0){
+        if($fileSize<1000000){
+          $fileNameNew = uniqid('',true).".".$fileActualExt;
+          $fileDestination = '../assets/img/'.$fileNameNew;
+          move_uploaded_file($fileTemporaryName,$fileDestination);
+        }else{
+          echo "arquivo muito grande";
+        }
+
+      }else{
+        echo "Houve um erro no upload do arquivo";
+      }
+
+    }else{
+      echo "Este arquivo não é compatível";
+    }
+  }
+
+  $nome = $_POST["nomeC"];
+  $preco = $_POST["preço"];
+  $descricao = $_POST["descricao"];
+  //$fileDestination = $_POST["file"];
+  $arquivo = $_GET['file'];
+
+   $id = $_GET['id'];
+
+
+     $resultado = "UPDATE produto SET nome_produto = '$nome',preco = '$preco', descricao = '$descricao',url_imagem = '$arquivo' WHERE id_produto='$id'";
+    $db_connect->query($resultado);
+
+
+
+
+ ?>
+  <!DOCTYPE html>
+  <html lang="en" dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
 
       <div class = "container">
           <div class ="row">
+   <div class="divAdmProd col-lg-5 col-md-5 mt-5 mb-5 text-center card">
 
-            <div class="divAdmProd col-lg-5 col-md-5 mt-3 mb-3 text-center card">
-             <img class="card-img-top" src="../assets/img/produtoHP2.png" alt="Produto 1">
-          <!--   <button class="col-lg-3 mt-3 mx-auto  mb-2 file btn btn-primary">Trocar foto</button>
-             		<input type="file" name="file"/>-->
-                <input class="col-lg-3 mt-3 " type="file" value="Input">
-            </div>
+      <form  action="AdminProduto.php?id=<?php echo $id; ?>&file=<?php echo $fileDestination;?> ?>" method="POST" enctype="multipart/form-data">
+        <img class="card-img-top" src="<?php echo $fileDestination; ?>" alt="Produto 1">
 
+           <input type="file" value="Input" name="file" ></input>
+           <button type="submit" name="submit" >Upload</button>
 
-            <form action="AdminProduto.php?id=<?php echo $id; ?>" method="POST" class="form-admin-produto">
-              <label for="nomeC">Nome:</label>
-              <input type="text" name="nomeC" id="nomeC" class="form-control"/required>
-              <label for="preço">Preço:</label>
-              <input type="text" name="preço" id="preço" class="form-control"/required>
+       </form>
+     </div>
 
-              <label for="descricao">Descrição:</label>
-              <textarea type="text" name="descricao" id="descricao" class="form-control field" rows="3" /required></textarea>
+     <form action="AdminProduto.php?id=<?php echo $id; ?>&file=<?php echo $fileDestination;?>" method="POST" class="form-admin-produto">
+       <label for="nomeC">Nome:</label>
+       <input type="text" name="nomeC" id="nomeC" class="form-control"/required>
+       <label for="preço">Preço:</label>
+       <input type="text" name="preço" id="preço" class="form-control"/required>
 
-               <button class="mt-3 btn btn-success " type="submit">Salvar</button>
+       <label for="descricao">Descrição:</label>
+       <textarea type="text" name="descricao" id="descricao" class="form-control field" rows="3" /required></textarea>
 
+        <button class="mt-3 btn btn-success " type="submit">Salvar</button>
 
-               <?php
+        <?php
 
+         ?>
+       </form>
 
-
-               $resultado = "UPDATE produto SET nome_produto = '$nome',preco = '$preco', descricao = '$descricao' WHERE id_produto=$id";
-
-          //     $resultadoArquivoImagem = "UPDATE produto SET url_imagem = $novo_nome WHERE id_produto=$id";
-
-
-               $db_connect->query($resultado);
-
-               echo $descricao;
-                ?>
+    </div>
+     <a class="mt-3 mb-3 btn btn-primary" href="produtosTeste.html" role="button">Voltar</a>
+  </div>
 
 
+    </body>
+  </html>
 
-              </form>
 
-            </div>
-
-               <a class="mt-3 mb-3 btn btn-primary" href="produtosTeste.html" role="button">Voltar</a>
-
-         </div>
 <?php
     include("footer-admin.php");
 ?>
