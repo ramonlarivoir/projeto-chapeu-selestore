de899db3f842d32b2a466f355d6f971f3dfdd75f
KemSasaoka <sasaoka.igor@gmail.com>
2018-06-24 10:33:16 -0300

    adicionado visualizador de fotos na pagina de tabela de produtos

--- html/tabela-produtos.php
+++ html/tabela-produtos.php
@@ -28,6 +28,9 @@ include("conexao.php");
                 <th scope="col">Produto</th>
                 <th scope="col">Preço</th>
                 <th scope="col">Descrição</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col"></th>
 
 
 
@@ -44,46 +47,77 @@ include("conexao.php");
                         $idProduto = $row['id_produto'];
                         $preco = $row['preco'];
                         $descricao = $row['descricao'];
+                        $imagem = $row['url-imagem'];
                         echo
                             '
                             <tr>
                                 <td>'.$idProduto.'</td>
-                                    <td>'.$nomeProduto.'</td>
-                                      <td>'.$preco.'</td>
-                                        <td>'.$descricao.'</td>
-                                    <td>
-                                        <a href="" role="button" data-toggle="modal" data-target="#excluirProduto'.$contador.'">
-                                            <img src="../assets/bootstrap/icons/png/trash-2x.png">
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a href="AdminProduto.php?id='.$idProduto.'">
-                                            <img src="../assets/bootstrap/icons/png/cog-2x.png">
-                                        </a>
+                                <td>'.$nomeProduto.'</td>
+                                <td>'.$preco.'</td>
+                                <td>'.$descricao.'</td>
+                                <td>
+                                    <a href="" role="button" data-toggle="modal" data-target="#exibirImagem'.$contador.'">
+                                        <img src="../assets/bootstrap/icons/png/eye-2x.png">
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="" role="button" data-toggle="modal" data-target="#excluirProduto'.$contador.'">
+                                        <img src="../assets/bootstrap/icons/png/trash-2x.png">
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="AdminProduto.php?id='.$idProduto.'">
+                                        <img src="../assets/bootstrap/icons/png/cog-2x.png">
+                                    </a>
                                 </td>
                             </tr>
-                            <div class="modal fade" id="excluirProduto'.$contador.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">AVISO</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-
-                                <div class="modal-body">
-                                    Tem certeza que deseja excluir este produto?
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                    <form method="post" action="tabela-produtos.php">
-                                        <button href="" type="submit" name ="id" value="'.$idProduto.'" class="btn btn-danger">Excluir produto</button>
-                                    </form>
-                                </div>
+                            <div class="modal fade" id="exibirImagem'.$contador.'" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                    
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="imageModalLabel">Imagem do Produto</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>      
+                                        </div>
+                                        
+                                        <div class="modal-body">
+                                            <img src= "'.$imagem.'" alt="imagem-do-produto-'.$idProduto.'">
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger" data-dismiss="modal">Voltar</button>
+                    
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
+
+
+
+                            <div class="modal fade" id="excluirProduto'.$contador.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">AVISO</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                    <div class="modal-body">
+                                        Tem certeza que deseja excluir este produto?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                        <form method="post" action="tabela-produtos.php">
+                                            <button href="" type="submit" name ="id" value="'.$idProduto.'" class="btn btn-danger">Excluir produto</button>
+                                        </form>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                             ';
                             $contador++;
--- html/tabela-usuarios.php
+++ html/tabela-usuarios.php
@@ -55,27 +55,27 @@
                                 </td>
                             </tr>
                             <div class="modal fade" id="excluirUsuario'.$contador.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">AVISO</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>      
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                    
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">AVISO</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>      
+                                        </div>
+                                        
+                                        <div class="modal-body">
+                                            Tem certeza que deseja excluir este usuário?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                                            <form method="post" action="tabela-usuarios.php">
+                                                <button href="" type="submit" name ="id" value="'.$idUsuario.'" class="btn btn-danger">Excluir Usuário</button>
+                                            </form>
+                                        </div>
+                                    </div>
                                 </div>
-                                
-                                <div class="modal-body">
-                                    Tem certeza que deseja excluir este usuário?
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                                    <form method="post" action="tabela-usuarios.php">
-                                        <button href="" type="submit" name ="id" value="'.$idUsuario.'" class="btn btn-danger">Excluir Usuário</button>
-                                    </form>
-                                </div>
-                                </div>
-                            </div>
                             </div>
                             ';
                             $contador++;
