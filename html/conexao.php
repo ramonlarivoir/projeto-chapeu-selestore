ed26ae22b1e7039fe8b814359efd92f798c66777
AndreLuiz123 <luizandr30@yahoo.com>
2018-06-24 23:00:03 -0300

    muda conexao.php para funcionar no meu pc

--- html/conexao.php
+++ html/conexao.php
@@ -1,12 +1,12 @@
 <?php
 
-$conexao = new mysqli("localhost","admin","admin","chapeuSelestore");
+$conexao = new mysqli("localhost","root","root","chapeuseletor","3306");
 mysqli_set_charset($conexao,"utf8");
 
 
 if (mysqli_connect_errno())
   {
-  echo 
+  echo
   '<div class="alert alert-danger text-center" role="alert">
       ERRO AO CONECTAR COM O BANCO DE DADOS: '. mysqli_connect_error() .'
   </div>';
