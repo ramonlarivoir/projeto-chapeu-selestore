7af7bd039812289c031b7f91b0de3554550563fb
AndreLuiz123 <luizandr30@yahoo.com>
2018-06-26 12:17:14 -0300

    add botao ao editar usuario e corrige cadastra categoria

--- html/admin/editar-usuario.php
+++ html/admin/editar-usuario.php
@@ -8,29 +8,37 @@
         $confirmarNovaSenha = $_POST['confirmacaoDaSenha'];
         $query = "UPDATE usuario SET login = '$novoUsuario', senha = MD5('$novaSenha') WHERE id_usuario = $id";
         if($novaSenha != $confirmarNovaSenha){
-            echo 
+            echo
                 '<div class="alert alert-danger text-center" role="alert">
                     AVISO: As senhas não combinam!!!
                 </div>';
         }else if(strlen($novaSenha)<4){
-            echo 
+            echo
                 '<div class="alert alert-danger text-center" role="alert">
                     AVISO: A senha é muito curta!!!
                 </div>';
         }else{
             $resultado = $conexao->query($query);
-            if($resultado){                
+            if($resultado){
                 echo
                     '<div class="alert alert-success text-center" role="alert">
                         USUÁRIO EDITADO COM SUCESSO!!!
-                    </div>';  
+                    </div>';
             }else{
-                echo 
+                echo
                     '<div class="alert alert-danger text-center" role="alert">
                       AVISO: ERRO AO TENTAR EDITAR USUÁRIO!!!
                      </div>';
             }
         }
+
+        echo '    <div class="col-3 mx-auto">
+                  <form action="tabela-usuarios.php">
+                <button type="submit" name="id" value = "'.$id.'" class="btn btn-primary ">Voltar à tabela de usuários</button>
+                  </form>
+            </div>';
+
+
     }
     if($_SERVER['REQUEST_METHOD']=='GET'){
         $id = $_GET['id'];
@@ -57,10 +65,10 @@
                         <button type="submit" name="id" value = "'.$id.'" class="btn btn-primary ">Enviar</button>
                     </div>
                 </form>
-            
+
             </div>
         ';
-        
+
     }
 
     include("footer-admin.php");
