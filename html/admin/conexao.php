013a511ab2aba00e1d9968bc4e5d936e5260a364
Lucas Pace <lucas.pace@engennharia.ufjf.br>
2018-06-26 22:28:09 -0300

    arrumando produtos individual

--- html/admin/conexao.php
+++ html/admin/conexao.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
-$conexao = new mysqli("localhost","admin","admin","chapeuselestore","3306");
-=======
 $conexao = new mysqli("localhost","admin","admin","chapeuSelestore","3306");
->>>>>>> 6c3005b239112ff8ec4c38be5ff1537493a22eba
+
 mysqli_set_charset($conexao,"utf8");
 
 
