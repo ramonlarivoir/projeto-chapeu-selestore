36c3fb91b401ea2bd89e5812702caa3a57396a12
KemSasaoka <sasaoka.igor@gmail.com>
2018-06-29 00:22:30 -0300

    resolvendo bug

--- html/valida.php
+++ html/valida.php
@@ -1,39 +1,20 @@
 <?php
-    session_start();
-
-    include("conexao.php");
-
-
-    if(!$conexao){
-        die("Falha na conexão: " . mysqli_conexaoect_error());
-    } else {
-
-    if((isset($_POST['email'])) && (isset($_POST['senha']))){
-        $email = mysqli_real_escape_string($conexao, $_POST['email']);
-        $senha = mysqli_real_escape_string($conexao, $_POST['senha']);
+	session_start();
+	include ('admin/conexao.php');
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
         $senha = md5($senha);
-
-        $sql = "SELECT * FROM usuario WHERE login = '$email' && senha = '$senha' LIMIT 1";
-        $result = mysqli_query($conexao, $sql);
-        $resultado = mysqli_fetch_assoc($result);
-
-        if(empty($resultado)) {
-            $_SESSION['loginErro'] = "Usuário ou senha inválido";
-            header("Location: ".$_SERVER['HTTP_REFERER']."");
-
-        }elseif(isset($resultado)){
-            $_SESSION['usuarioLogin'] = $resultado['login'];
+        $query = "SELECT * FROM usuario WHERE login = '$email' AND senha = '$senha'";
+		$resultado = $conexao->query($query);
+		$row = $resultado->fetch_assoc();
+        //$resultado = mysqli_fetch_assoc($result);
+        if (empty($resultado)) {
+            $_SESSION['loginErro'] = "mandado";
+            header("Location: index.php");
+        } else{
+            $_SESSION['usuarioLogin'] = $row['login'];
             header("Location: admin/tabela-produtos.php");
-        }else{
-            $_SESSION['loginErro'] = "Usuário ou senha inválido";
-            header("Location: ".$_SERVER['HTTP_REFERER']."");
         }
-
-
-    } else {
-        $_SESSION['loginErro'] = "Usuário ou senha inválido";
-        header("Location: ".$_SERVER['HTTP_REFERER']."");
-    }
-}
-
+	}
 ?>
